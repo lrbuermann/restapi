d50bb08d4eb41df29711b3268d6fa2b33c260f7f
Lee Buermann <lrbuermann@gmail.com>
2019-12-01 17:55:06 -0600

    adding spring profiles adding additional config for aws pipeline

--- src/test/java/lrbuermann/target/restapi/RestapiApplicationTests.java
+++ src/test/java/lrbuermann/target/restapi/RestapiApplicationTests.java
@@ -2,8 +2,10 @@ package lrbuermann.target.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "local")
 class RestapiApplicationTests {
 
 	@Test
